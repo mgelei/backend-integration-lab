f289d87841ebd9e10e814af0998a22053d124b66
Mate Gelei-Szego <34216064+mgelei@users.noreply.github.com>
2023-06-20 22:16:41 +0200

    EF migrations for #21

--- Lottery.DataAccess/Migrations/20230620201501_Draws_CreatedAt_Index.Designer.cs
+++ Lottery.DataAccess/Migrations/20230620201501_Draws_CreatedAt_Index.Designer.cs
@@ -4,6 +4,7 @@ using Lottery.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lottery.DataAccess.Migrations
 {
     [DbContext(typeof(LotteryDbContext))]
-    partial class LotteryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230620201501_Draws_CreatedAt_Index")]
+    partial class Draws_CreatedAt_Index
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Lottery.DataAccess/Migrations/20230620201501_Draws_CreatedAt_Index.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lottery.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Draws_CreatedAt_Index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_DrawHistory_CreatedAt",
+                table: "DrawHistory",
+                column: "CreatedAt",
+                descending: new bool[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DrawHistory_CreatedAt",
+                table: "DrawHistory");
+        }
+    }
+}
