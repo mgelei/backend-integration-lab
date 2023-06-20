5fd395be3d43975dccda3671ad7078895e530a86
Mate Gelei-Szego <34216064+mgelei@users.noreply.github.com>
2023-06-20 17:01:49 +0200

    Create EF context fixes #11 fixes #12

--- Lottery.Model/Draw.cs
+++ Lottery.Model/Draw.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Lottery.Model.Validations;
 
 namespace Lottery.Model;
 
+[Table("DrawHistory")]
 public class Draw
 {
     public int Id { get; set; }
